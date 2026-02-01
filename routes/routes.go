739ca254c3b6be34f1d83d8242ec55728f75ced6
Thomas Queiroz <thomas.queiroz+ideal@idealctvm.com.br>
2026-02-01 14:51:03 -0300

    feat: upping postgresql using docker-compose

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,7 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()		   								 //criando uma nova rota com mux
 	r.HandleFunc("/", controllers.Home)                          // quando for feito uma requisição para a rota "/", chama a função Home do controllers
-	r.HandleFunc("/personalities", controllers.AllPersonalities) // quando for feito uma requisição para a rota "/personalities", chama a função AllPersonalities do controllers
-	r.HandleFunc("/personalities/{id}", controllers.GetPersonalityById)
-	log.Fatal(http.ListenAndServe(":8080", r))                   //subindo o servidor na porta 8080
+	r.HandleFunc("/api/personalities", controllers.AllPersonalities) // quando for feito uma requisição para a rota "/api/personalities", chama a função AllPersonalities do controllers
+	r.HandleFunc("/api/personalities/{id}", controllers.GetPersonalityById) // rota para obter personalidade por ID
+	log.Fatal(http.ListenAndServe(":8081", r))                   //subindo o servidor na porta 8080
 }
